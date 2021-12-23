d2f1e68250ee1d9db033d1bcb8f5db7eb4c901b4
moritz1029 <81555199+moritz1029@users.noreply.github.com>
2021-12-23 23:17:28 +0100

    one mod for all supported mc versions

--- src/main/java/me/voidxwalker/peaceful/mixin/DifficultyMixin.java
+++ src/main/java/me/voidxwalker/peaceful/mixin/DifficultyMixin.java
@@ -1,17 +1,21 @@
 package me.voidxwalker.peaceful.mixin;
 
 import net.minecraft.world.Difficulty;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(Difficulty.class)
-public class DifficultyMixin {
+public class DifficultyMixin  {
+    @Shadow @Final private static Difficulty[] BY_NAME;
+
     @Inject(method = "byOrdinal", at = @At("RETURN"),cancellable = true)
     private static void noPeaceful(int ordinal, CallbackInfoReturnable<Difficulty> cir){
         if(cir.getReturnValue().equals(Difficulty.PEACEFUL)){
-            cir.setReturnValue(cir.getReturnValue().cycle());
+             cir.setReturnValue(BY_NAME[1]);
         }
     }
 }
